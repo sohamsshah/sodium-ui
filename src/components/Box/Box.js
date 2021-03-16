b10a89527e843669a0182b61cfcd39b47184c8bb
sohamsshah <sohamshah456@gmail.com>
2021-03-17 01:05:55 +0530

    add more props

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -4,7 +4,7 @@ function Box(props){
     const {m = "0", p = "1rem", w, h, color, bgcolor, children, ...rest} = props;
 
     return (
-        <div style={{padding: `${p}`, margin:`${m}`, color: `${color}`, backgroundColor:`${bgcolor}`}} {...rest}>
+        <div style={{padding: `${p}`, margin:`${m}`, color: `${color}`, backgroundColor:`${bgcolor}`, width:`${w}`, height:`${h}`}} {...rest}>
             {children}
         </div>
     )
--- src/components/Box/Box.stories.js
+++ src/components/Box/Box.stories.js
@@ -5,6 +5,16 @@ import Box from "./Box";
 export default {
     title: 'Sodium UI/Box', //mandatory
     component:Box,
+    argsTypes:{
+        h: {control: "text"},
+        w: {control: "text"},
+        p: {control: "text"},
+        m: {control: "text"},
+        color: {control: "text"},
+        bgcolor: {control: "text"},
+    
+
+    }
 }
 
 const Template = args => <Box {...args} />
@@ -12,7 +22,10 @@ const Template = args => <Box {...args} />
 export const Standard = Template.bind({})
 Standard.args = {
     children: <div>Hello World</div>,
-    // p: "2rem",
+    p: "2rem",
     m:"1rem",
-    bgcolor:"pink"
+    bgcolor:"yellow",
+    color: "#212121",
+    h: "50%",
+    w: "50%"
 }
