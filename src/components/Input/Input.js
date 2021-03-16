d813dea5d087876d3d974d195fa17e31e715ccb3
sohamsshah <sohamshah456@gmail.com>
2021-03-17 01:35:20 +0530

    update code

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import "./Input.css";
 
 function Input(props){
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Button from "./components/Button/Button"
-import Avatar from "./components/Button/Button"
-import Box from "./components/Button/Button"
-import Input from "./components/Button/Button"
+import Avatar from "./components/Avatar/Avatar"
+import Box from "./components/Box/Box"
+import Input from "./components/Input/Input"
 
 export {Button, Avatar, Box, Input}
