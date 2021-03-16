05d0604b3ef41ea0b55ddebe0f1bc2de64f584a1
sohamsshah <sohamshah456@gmail.com>
2021-03-17 01:07:39 +0530

    export components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import Button from "./components/Button/Button"
+import Avatar from "./components/Button/Button"
+import Box from "./components/Button/Button"
+import Input from "./components/Button/Button"
 
-export {Button}
+export {Button, Avatar, Box, Input}
