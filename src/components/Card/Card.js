dc093bb6743e87f16640f1e8831393ac08a03f60
sohamsshah <sohamshah456@gmail.com>
2021-03-19 18:45:05 +0530

    start working for card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,19 @@
 import React from "react"
 import Paper from "./../Paper/Paper"
 import Button from "./../Button/Button"
+import Typography from "./../Typography/Typography"
 
 export default function Card(){
     return(
         <Paper style={{width:"250px", height:"300px"}}>
-            <h1 style={{padding:"1rem"}}>
-                Hello World
-            </h1>
+        <Typography variant="h4">
+            Card Title
+        </Typography>  
+        <Typography variant="body1">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        </Typography>
+        <Button> Visit</Button>
+        
         </Paper>
     )
 }
--- src/components/Typography/Typography.js
+++ src/components/Typography/Typography.js
@@ -3,22 +3,22 @@ import "./Typography.css"
 
 
 export default function Typography(props){
-    const {variant="body1", display="initial", align="inherit", children, ...rest} = props;
+    const {variant="body1", display="block", align="inherit", children, ...rest} = props;
     return(
         {
-            "h1": <h1 className="na-h1" style={{display:`${display}`, textAlign:`${align}` }} {...rest}>{children}</h1>,
-            "h2": <h2 className="na-h2" style={{display:`${display}`, textAlign:`${align}` }} {...rest}>{children}</h2>,
-            "h3": <h3 className="na-h3" style={{display:`${display}`, textAlign:`${align}` }} {...rest}>{children}</h3>,
-            "h4": <h4 className="na-h4" style={{display:`${display}`, textAlign:`${align}` }} {...rest}>{children}</h4>,
-            "h5": <h5 className="na-h5" style={{display:`${display}`, textAlign:`${align}` }} {...rest}>{children}</h5>,
-            "h6": <h6 className="na-h6" style={{display:`${display}`, textAlign:`${align}` }} {...rest}>{children}</h6>,
-            "body1": <p className="na-body1" style={{display:`${display}`, textAlign:`${align}`}} {...rest}>{children}</p>,
-            "body2": <p className="na-body2" style={{display:`${display}`, textAlign:`${align}`}} {...rest}>{children}</p>,
-            "subtitle1": <h6 className="na-subtitle1" style={{display:`${display}`, textAlign:`${align}`}} {...rest}>{children}</h6>,
-            "subtitle2": <h6 className="na-subtitle2" style={{display:`${display}`, textAlign:`${align}`}} {...rest}>{children}</h6>,
-            "button-text": <span className="na-button-text" style={{display:`${display}`, textAlign:`${align}`}} {...rest}>{children}</span>,
-            "caption-text": <span className="na-caption-text" style={{display:`${display}`, textAlign:`${align}`}} {...rest}>{children}</span>,
-            "overline-text": <span className="na-overline-text" style={{display:`${display}`, textAlign:`${align}`}} {...rest}>{children}</span>
+            "h1": <h1 className="na-h1 na-typography" style={{display:`${display}`, textAlign:`${align}` }} {...rest}>{children}</h1>,
+            "h2": <h2 className="na-h2 na-typography" style={{display:`${display}`, textAlign:`${align}` }} {...rest}>{children}</h2>,
+            "h3": <h3 className="na-h3 na-typography" style={{display:`${display}`, textAlign:`${align}` }} {...rest}>{children}</h3>,
+            "h4": <h4 className="na-h4 na-typography" style={{display:`${display}`, textAlign:`${align}` }} {...rest}>{children}</h4>,
+            "h5": <h5 className="na-h5 na-typography" style={{display:`${display}`, textAlign:`${align}` }} {...rest}>{children}</h5>,
+            "h6": <h6 className="na-h6 na-typography" style={{display:`${display}`, textAlign:`${align}` }} {...rest}>{children}</h6>,
+            "body1": <p className="na-body1 na-typography" style={{display:`${display}`, textAlign:`${align}`}} {...rest}>{children}</p>,
+            "body2": <p className="na-body2 na-typography" style={{display:`${display}`, textAlign:`${align}`}} {...rest}>{children}</p>,
+            "subtitle1": <h6 className="na-subtitle1 na-typography" style={{display:`${display}`, textAlign:`${align}`}} {...rest}>{children}</h6>,
+            "subtitle2": <h6 className="na-subtitle2 na-typography" style={{display:`${display}`, textAlign:`${align}`}} {...rest}>{children}</h6>,
+            "button-text": <span className="na-button-text na-typography" style={{display:`${display}`, textAlign:`${align}`}} {...rest}>{children}</span>,
+            "caption-text": <span className="na-caption-text na-typography" style={{display:`${display}`, textAlign:`${align}`}} {...rest}>{children}</span>,
+            "overline-text": <span className="na-overline-text na-typography" style={{display:`${display}`, textAlign:`${align}`}} {...rest}>{children}</span>
         }[variant] || <p className="na-body1" style={{display:`${display}`, textAlign:`${align}`}} {...rest}>{children}</p> 
     )
 }
