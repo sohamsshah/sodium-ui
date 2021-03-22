464aed827e1f2bf442c6fda5436ffcf643ce2451
sohamsshah <sohamshah456@gmail.com>
2021-03-22 16:45:05 +0530

    export the components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import Button from "./components/Button/Button"
 import Avatar from "./components/Avatar/Avatar"
 import Box from "./components/Box/Box"
+import Alert from "./components/Alert/Alert"
+import List from "./components/List/List"
+import Card from "./components/Card/Card"
+import Modal from "./components/Modal/Modal"
+import Paper from "./components/Paper/Paper"
+import Toast from "./components/Toast/Toast"
+import Spinner from "./components/Spinner/Spinner"
+import Typography from "./components/Typography/Typography"
 import Input from "./components/Input/Input"
 
-export {Button, Avatar, Box, Input}
+export {Button, Avatar, Box, Input, Alert, List, Card, Modal, Paper, Spinner, Toast, Typography}
